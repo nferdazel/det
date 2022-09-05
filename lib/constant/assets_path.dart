aa4c6331a9e005e3a30edcf086090c8bf5e7735c
Fredianto <fredinix@proton.me>
2022-09-05 15:14:45 +0700

    (assets): configuring asset path for easier import

---
+++ lib/constant/assets_path.dart
@@ -0,0 +1,10 @@
+part of 'constant.dart';
+
+class Lanes {
+  static String email = 'assets/illustrations/email_is_on_the_way.svg';
+  static String export = 'assets/illustrations/export_data.svg';
+  static String planning = 'assets/illustrations/planning_ahead.svg';
+  static String control =
+      'assets/illustrations/total_control_of_your_money.svg';
+  static String money = 'assets/illustrations/where_your_money_goes.svg';
+}
---
+++ lib/constant/constant.dart
@@ -0,0 +1,1 @@
+part 'assets_path.dart';
